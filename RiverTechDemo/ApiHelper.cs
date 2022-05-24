6f67bff78045d9176c4ea143d40d0f6aef582a90
geomal <malliageorge97@gmail.com>
2022-05-24 17:18:53 +0200

    Api Test cases

--- RiverTechDemo/ApiHelper.cs
+++ RiverTechDemo/ApiHelper.cs
@@ -24,10 +24,10 @@ namespace RiverTechDemo
             new MediaTypeWithQualityHeaderValue("application/json"));
         }
 
-        public async Task<UserModel> CallApi(int userId)
+        public async Task<UserModel> CallApiUsingGet(string userId)
         {
             string url = $"https://jsonplaceholder.typicode.com/users/{ userId }";
-            
+           
             using (HttpResponseMessage response = await Client.GetAsync(url))
             {
                 if (response.IsSuccessStatusCode)
@@ -40,6 +40,31 @@ namespace RiverTechDemo
                     throw new Exception(response.ReasonPhrase);
                 }
             }
+            
+        }
+
+        public async Task<UserModel> CallApiUsingPost(string userId)
+        {
+            string url = $"https://jsonplaceholder.typicode.com/users/{ userId }";
+            var values = new Dictionary<string, string>
+                          {
+                              { userId, userId },
+                          };
+            var content = new FormUrlEncodedContent(values);
+
+            using (HttpResponseMessage response = await Client.PostAsync(url, content).ConfigureAwait(false))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var userModel = await response.Content.ReadAsAsync<UserModel>();
+                    return userModel;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+
         }
 
         public async Task<HttpResponseMessage> GetApiResponseStatus(int userId)
